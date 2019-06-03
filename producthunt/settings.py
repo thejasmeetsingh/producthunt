42c7d4b906fd2dad4dc5ca147f2c19c23279f846
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2019-06-03 23:39:25 +0530

    Added Product and Account apps

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product.apps.ProductConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
